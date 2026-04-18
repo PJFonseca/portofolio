a6d27ad4a889330742a998c44ec51a344c633505
Pedro Fonseca <pedro+github@fonseca.com.pt>
2026-04-18 14:55:43 +0000

    Things done in the Classroom

--- escola/views.py
+++ escola/views.py
@@ -9,4 +9,4 @@ def cursos_view(request):
 
     cursos = Curso.objects.select_related('professor').prefetch_related('alunos').all()
     
-    return render(request, 'cursos_online.html', {'cursos': cursos})
+    return render(request, 'escola/cursos.html', {'cursos': cursos})
--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include              #     <- adicionar include 
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,5 +9,4 @@ urlpatterns = [
 ]
 
 # Serve ficheiros media em DEBUG
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG: urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
